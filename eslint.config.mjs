e8ebbb9154fc520f3796ef67a8ebf095c2d57495
Senator <senatorcox90@gmail.com>
2025-05-08 18:44:28 +0100

    Initial Commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,9 +11,9 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    "@typescript-eslint/no-this-alias": "off",
-  },
+  // {
+  //   "@typescript-eslint/no-this-alias": "off",
+  // },
 ];
 
 export default eslintConfig;
