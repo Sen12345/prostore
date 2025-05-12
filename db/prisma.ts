b79d915261443245fbba7cc785c433bcddc0ae1a
Senator <senatorcox90@gmail.com>
2025-05-12 14:15:03 +0100

    Fixed database connection issue with Prisma Neon and Pool conflict

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,15 +1,19 @@
-import { neonConfig } from "@neondatabase/serverless";
+import { neonConfig, neon } from "@neondatabase/serverless";
 import { PrismaNeon } from "@prisma/adapter-neon";
-import { PrismaClient } from "@/lib/generated/prisma/client";
+import { PrismaClient } from "@/lib/generated/prisma";
+
 import ws from "ws";
 
 // Sets up WebSocket connections, which enables Neon to use WebSocket communication.
 neonConfig.webSocketConstructor = ws;
 const connectionString = `${process.env.DATABASE_URL}`;
 
-// Creates a new connection pool using the provided connection string, allowing multiple concurrent connections.
-// const pool = new Pool({ connectionString });
 const adapter = new PrismaNeon({ connectionString });
+// export const prisma = new PrismaClient({ adapter });
+
+// Creates a new connection pool using the provided connection string, allowing multiple concurrent connections.
+
+// const pool = new Pool({ connectionString });
 
 // Instantiates the Prisma adapter using the Neon connection pool to handle the connection between Prisma and Neon.
 // const adapter = new PrismaNeon(pool);
